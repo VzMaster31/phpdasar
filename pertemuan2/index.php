60e3b50c969479562e58bdba1b1239ef49afc702
Rizky Pamungkas <ah067622@gmail.com>
2021-08-19 11:52:14 +0700

    Menambahkan file pertemuan 1, 2, & 4

---
+++ pertemuan2/index.php
@@ -0,0 +1,25 @@
+<?php 
+// Pertemuan 2 - Sintaks PHP
+
+// Penulisan sintaks PHP
+// 1. PHP di dalam HTML (Recomend)
+// 2. HTML di dalam PHP (tdk Recomend)
+
+// Variabel
+// Tidak boleh diawali angka tapi boleh mengandung angka
+
+// $nama = "Rizky";
+
+// echo $nama;
+
+// Operator
+// aritmatika
+// + - * / %
+
+// Penggabung string / concatenation / concat
+// .
+$nama_depan = "Rizky";
+$nama_belakang = "Pamungkas";
+echo $nama_depan . " " . $nama_belakang;
+
+ ?>
---
+++ pertemuan4/date.php
@@ -0,0 +1,24 @@
+<?php
+    // date
+    //untuk menampilkan tanggal dengan format
+    // tertentu
+
+    // echo date("l, d-M-Y");
+
+    // time
+    // UNIX Timestamp / EPOCH time
+    // detik yang sudah berlalu sejak 1 januari 1970
+    // echo time();
+
+    // echo date("l, d M Y", time()-60*60*24*100);
+
+    // mktime
+    // membuat sendiri detik dari UNIX time
+    // mktime(0,0,0,0,0,0)
+    // jam, menit, detik, bulan, tanggal, tahun
+    // echo date("l", mktime(0,0,0,21,2,2003));
+
+    //strtotime
+    echo date("l", strtotime("21 feb 2003"));
+
+?>
